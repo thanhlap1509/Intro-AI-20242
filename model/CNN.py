3f762482aec670ecc9c548afd61f2609f5ad1316
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2025-03-20 14:34:04 +0700

    add output size to cnn

--- model/CNN.py
+++ model/CNN.py
@@ -2,18 +2,22 @@ import torch
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self, kernel_size, num_filters, kernel_stride, pool_size, pool_stride):
+    def __init__(self, input_size, kernel_size, num_filters, pool_size, kernel_stride = 1, pool_stride = 1):
         super().__init__()
         
+        self.input_size = input_size
         self.filters = torch.nn.Conv1d(in_channels = 1, 
                                        out_channels = num_filters, 
                                        kernel_size = kernel_size, 
                                        stride = kernel_stride,
-                                       padding = kernel_size // 2) # for same size padding, require kernel_size odd
+                                       padding = kernel_size // 2) # for same size padding, require kernel_size odd and stride = 1
         
-        self.max_pool = nn.MaxPool1d(kernel_size=pool_size, 
-                                     stride=pool_stride, 
-                                     padding = pool_size // 2) # for same padding, require pool_size odd
+        self.max_pool = nn.MaxPool1d(kernel_size = pool_size, 
+                                     stride = pool_stride, 
+                                     padding = 0) 
+        
+        # Since we apply the same padding for convolution op, the output size is: 
+        self.output_size = num_filters * (((input_size - pool_size) / pool_stride) + 1) 
         
     def forward(self, X):
     
@@ -23,11 +27,11 @@ class CNN(nn.Module):
             X       (seq_len, input_size): Input sequence data, seq_len is number of time step, input_size is size of feature vector
             
         Returns:
-            X       (seq_len, num_filters * L_out): higher order representation of input data
+            X       (seq_len, num_filters * L_pooled): higher order representation of input data
         
         """
-        X = X.unsqueeze(dim=1)  # X = (seq_len, 1, input_size) = (N, C_in, L) for convolution 
-        X = self.filters(X)     # X = (seq_len, num_filters, L_out)
-        X = self.max_pool(X)    # X = (seq_len, num_filters, l_pooled)
-        X = torch.flatten(X, start_dim = 1) # X = (seq_len, num_filters * L_pooled) concat hidden feature vector from different filters to form a singular vector per time step
+        X = X.unsqueeze(dim=1)  # X = (seq_len, 1, input_size) 
+        X = self.filters(X)     # X = (seq_len, num_filters, input_size) = (N, C_in, L_out) 
+        X = self.max_pool(X)    # X = (seq_len, num_filters, ((input_size - pool_size) / pool_stride) + 1) 
+        X = torch.flatten(X, start_dim = 1) # X = (seq_len, num_filters * ((input_size - pool_size) / pool_stride) + 1)) 
         return X
